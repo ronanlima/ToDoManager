867b3ada6652d570413099bc9af9a5d2829e04ab
ronan.lima <ronan.lima@sicoob.com.br>
2020-05-10 14:33:38 -0300

    Exibindo layout das tarefas concluídas

--- src/screens/DoneTasks.js
+++ src/screens/DoneTasks.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
+import {TaskListView} from '../components/Components';
+import {readTaskFromFirebaseAsync} from '../services/firebaseApi';
 
 const imgDone = require('../assets/icon-done-light.png');
 
 export default class DoneTasks extends Component {
+    state = {
+        tasks: [],
+    };
+
+    componentDidMount() { 
+        readTaskFromFirebaseAsync(this._fetchTasks.bind(this));
+    }
+    _fetchTasks(tasks) {
+        const tasksToDo = tasks.filter(t => t.isDone); 
+        this.setState({tasks: tasksToDo});
+    }
+
     render() {
         return (
-            <View style={ styles.conteiner }/> 
+            <View style={ styles.conteiner }> 
+                <TaskListView tasks={this.state.tasks} />
+            </View>
         );
     }
 };
